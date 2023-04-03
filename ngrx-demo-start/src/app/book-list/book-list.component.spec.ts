e41fb18df477bb175a87e1bfbf8faaaf9e912893
Remco Nonhebel <nonhebel.r@tkppensioen.nl>
2023-04-03 11:27:04 +0200

    Add jest unit tests

--- ngrx-demo-start/src/app/book-list/book-list.component.spec.ts
+++ ngrx-demo-start/src/app/book-list/book-list.component.spec.ts
@@ -1,23 +1,25 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { Spectator, createComponentFactory } from '@ngneat/spectator';
 import { BookListComponent } from './book-list.component';
+import { ComponentFixture } from '@angular/core/testing';
 
 describe('BookListComponent', () => {
+  let spectator: Spectator<BookListComponent>;
   let component: BookListComponent;
   let fixture: ComponentFixture<BookListComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ BookListComponent ]
-    })
-    .compileComponents();
+  const createComponent = createComponentFactory(BookListComponent);
 
-    fixture = TestBed.createComponent(BookListComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+  beforeEach(() => {
+    spectator = createComponent();
+    component = spectator.component;
+    fixture = spectator.fixture;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should match snapshot', () => {
+    expect(fixture).toMatchSnapshot();
   });
 });
